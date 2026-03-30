8addb45e28a82e89110bb780884b9588e9f058c4
MorandiCorreia <gmnc.academico@gmail.com>
2026-03-29 23:01:36 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,10 @@
+#include <stdio.h>               //Inclui a biblioteca stdio.h, que contém funções como printf
+
+int main(){                      //"main" é o nome da função. E o bloco de código começa com { e termina com }
+    printf("Olá, Mundo!\n");     //printf é "printar na tela"
+    //Texto sempre entre aspas. E o \n é para dar ENTER
+    //Ponto e vírgula indica o fim de uma instrução
+
+    return 0;                    //Boa prática em C pra encerrar o cód.
+}                                //Nunca esquecer de fechar colchetes
 
